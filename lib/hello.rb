416b7ca7ffec697d07732b7676eb7442e87aaa78
Ty Raino <mstymetris@yahoo.com>
2020-03-12 07:35:01 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,11 +5,7 @@ while i < array.length
   yield array[i]
   i += 1
 end
+array
 end
 
 # call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
